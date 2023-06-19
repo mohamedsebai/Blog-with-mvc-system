55b496020a411a08e0be71c4d11ba04f31d4b743
Mohamed sebai <mohamedseabeai@gmail.com>
2023-06-19 15:36:20 +0300

    Update reset_password.php

--- app/controllers/reset_password.php
+++ app/controllers/reset_password.php
@@ -41,8 +41,8 @@ class reset_password extends Framework{
              $mail->isSMTP();                                           //Send using SMTP
              $mail->Host       = 'smtp.titan.email';                    //Set the SMTP server to send through
              $mail->SMTPAuth   = true;                                  //Enable SMTP authentication
-             $mail->Username   = 'mohamedseabeai@arrogantm.com';                     //SMTP username
-             $mail->Password   = 'Moh12()*';                               //SMTP password
+             $mail->Username   = 'email@company.com';                     //SMTP username
+             $mail->Password   = 'password';                               //SMTP password
              $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
              $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
